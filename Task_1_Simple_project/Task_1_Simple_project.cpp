5931d4a1d6497e6dca085f24054f1a65cb44598e
Scorpic <volkadav99@list.ru>
2023-01-31 14:38:07 +0300

    initial commit

---
+++ Task_1_Simple_project/Task_1_Simple_project.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL,"Rus");
+    std::string my_name;
+    std::cout << "Введите имя: \n";
+    std::cin >> my_name;
+    std::cout << "Здравствуйте, " << my_name << "!" << std::endl;
+    return 0;
+
+}
---
+++ Task_2_The_project_is_more_complicated/Task_2_The_project_is_more_complicated.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include "Shapes.h"
+#include "Triangle.h"
+#include "RectangularTriangle.h"
+#include "IsoscelesTriangle.h"
+#include "EquilateralTriangle.h"
+#include "Quadrilateral.h"
+#include "Square.h"
+#include "Rectangle.h"
+#include "Parallelogram.h"
+#include "Rhomb.h" //Shapes.h Triangle.h RectangularTriangle.h IsoscelesTriangle.h EquilateralTriangle.h Quadrilateral.h Square.h Rectangle.h Parallelogram.h Rhomb.h
+//Shapes.cpp Triangle.cpp RectangularTriangle.cpp IsoscelesTriangle.cpp EquilateralTriangle.cpp Quadrilateral.cpp Square.cpp Rectangle.cpp Parallelogram.cpp Rhomb.cpp
+
+
+void print_shapes(Shapes* shapes) {
+    int a = 6, b = 26, c = 64, A = 7, B = 340, C = 45;
+
+    RectangularTriangle objRT(a, b, c, A, B);
+    shapes = &objRT;
+    shapes->printFigure();
+    IsoscelesTriangle objIT(a, b, 3, 3, 3);
+    shapes = &objIT;
+    shapes->printFigure();
+    EquilateralTriangle objET(61);
+    shapes = &objET;
+    shapes->printFigure();
+    Rectangle objRec(2, 2);
+    shapes = &objRec;
+    shapes->printFigure();
+    Square objSqu(5);
+    shapes = &objSqu;
+    shapes->printFigure();
+    Parallelogram objPar(3, 45, 6, 45);
+    shapes = &objPar;
+    shapes->printFigure();
+    Rhomb objRmb(a, b, c, 9, 55, 66);
+    shapes = &objRmb;
+    shapes->printFigure();
+
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+
+    Shapes* shapes = new Shapes;
+
+    print_shapes(shapes);
+
+}
